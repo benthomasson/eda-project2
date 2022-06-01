bd93ed56178acbf6c43ffb51e91948a0844906b9
Ben Thomasson <bthomass@redhat.com>
2022-06-01 09:54:49 -0400

    Adds quick tests

--- quick_test.py
+++ quick_test.py
@@ -4,8 +4,20 @@
 from ansible_events_ui.manager import activate_rulesets
 import asyncio
 
+async def read_output(proc):
+
+    done = False
+    while not done:
+        line =  await proc.stdout.readline()
+        if len(line) == 0:
+            break
+        line = line.decode()
+        print(f'{line}', end='')
+
 
 async def main():
+
+    print('start')
 
     with open('hello_events.yml') as f:
         rules = f.read()
@@ -13,7 +25,19 @@ async def main():
         inventory = f.read()
     with open('vars.yml') as f:
         extravars = f.read()
-    await activate_rulesets('quay.io/bthomass/events-demo-ci-cd:latest', rules, inventory, extravars)
+    print('activate')
+    cmd, proc = await activate_rulesets('quay.io/bthomass/events-demo-ci-cd:latest', rules, inventory, extravars)
+    print('running')
 
+    print(f'pid {proc.pid}')
+
+    task1 = asyncio.create_task(read_output(proc))
+
+    await proc.wait()
+    stdout, stderr = await proc.communicate()
+
+    print(f'[{cmd!r} exited with {proc.returncode}]')
+
+    await task1
 
 asyncio.run(main())
